5bf7e9d092c2b34440159201d670be8e5d220283
xlrrr <ss.staykov@gmail.com>
2018-04-27 15:05:54 +0300

    + added test scene for using navmesh

---
+++ Assets/Source/Entity/PathFinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class PathFinder : MonoBehaviour {
+
+	public Transform Destination;
+
+    private NavMeshAgent m_agent;
+
+	void Start () {
+		m_agent = GetComponent<NavMeshAgent> ();
+	}
+
+	void Update () {
+		if(Destination && (m_agent && m_agent.destination != Destination.position))
+        {
+			m_agent.SetDestination(Destination.position);
+        }
+	}
+}
---
+++ Assets/Source/Input/TouchController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	public GameObject Team1;
+	public GameObject Team2;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetMouseButtonDown (0) || Input.GetMouseButtonDown (1)){ 
+			RaycastHit hit; 
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+			if ( Physics.Raycast (ray,out hit,100.0f)  && hit.transform.gameObject.tag == "Tower") {
+				if (Input.GetMouseButton (0)) 
+				{
+					for (int i = 0; i < Team1.transform.childCount; i++) {
+						Team1.transform.GetChild (i).GetComponent<PathFinder> ().Destination = hit.transform;
+					}
+				}
+				else if (Input.GetMouseButtonDown (1))
+				{
+					for (int i = 0; i < Team1.transform.childCount; i++) {
+						Team2.transform.GetChild (i).GetComponent<PathFinder> ().Destination = hit.transform;
+					}
+				}
+					
+			}
+		}
+	}
+}
